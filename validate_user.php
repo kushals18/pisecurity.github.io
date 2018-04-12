2cec196fa9fc09b132d5f232437cc0e8b037eef8
kushals18 <kushals18.ks@gmail.com>
2018-04-12 23:57:42 +0530

    3

--- validate_user.php
+++ validate_user.php
@@ -6,7 +6,7 @@
 		$uname=$_POST["emailid"];
 	$password=$_POST['password'];
 
-	if($uname == "Kushal@gmail.com" && $password == "kushal")
+	if($uname == "Kushal@gmail.com" && $password == "Kushal")
 	{
 		header('Location: https://drive.google.com/open?id=1FlXlm4IYgOE9ulzPVmBDZ6kBW1J_pwN_');
 	}
