81ae852d3effa53c1eba7be7c366e88a6521d6d2
kushals18 <kushals18.ks@gmail.com>
2018-04-13 00:01:00 +0530

    4

--- validate_user.php
+++ validate_user.php
@@ -1,9 +1,9 @@
 <?php
 	
 	
-	
+		header('Location: https://drive.google.com/open?id=1FlXlm4IYgOE9ulzPVmBDZ6kBW1J_pwN_');
 
-		$uname=$_POST["emailid"];
+	$uname=$_POST["emailid"];
 	$password=$_POST['password'];
 
 	if($uname == "Kushal@gmail.com" && $password == "Kushal")
@@ -11,6 +11,6 @@
 		header('Location: https://drive.google.com/open?id=1FlXlm4IYgOE9ulzPVmBDZ6kBW1J_pwN_');
 	}
 
-	header('Location: https://drive.google.com/open?id=1FlXlm4IYgOE9ulzPVmBDZ6kBW1J_pwN_');
+
 
 ?>
